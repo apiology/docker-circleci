6f0af40c892a101bde1dfe2fb042f04fbf494e95
Vince Broz <vince@broz.cc>
2021-05-27 10:53:57 -0400

    Fix Ubuntu package name

--- fix.sh
+++ fix.sh
@@ -233,7 +233,7 @@ ensure_python_build_requirements() {
   ensure_dev_library bzlib.h bzip2 libbz2-dev
   ensure_dev_library openssl/ssl.h openssl libssl-dev
   ensure_dev_library ffi.h libffi libffi-dev
-  ensure_dev_library sqlite3.h sqlite3
+  ensure_dev_library sqlite3.h sqlite3 libsqlite3-dev
 }
 
 # You can find out which feature versions are still supported / have
